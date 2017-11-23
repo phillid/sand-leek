b452860634f5b219d11a85a824786ef91731fee7
David Phillips <david@sighup.nz>
2017-11-23 22:26:15 +1300

    Quick hack to protect against shortening numbers

--- sand-leek.c
+++ sand-leek.c
@@ -290,7 +290,7 @@ monitor_progress(unsigned long volatile *khashes, int thread_count) {
 		hours   = delta % 24; delta -= hours  ; delta /= 24;
 		days    = delta % 24;
 
-		iprintf("[%02d:%02d:%02d:%02d]: %.2f %s hashes%s. Now ~%lu kH/s (%.2f kH/s/thread)\r",
+		iprintf("[%02d:%02d:%02d:%02d]: %.2f %s hashes%s. Now ~%lu kH/s (%.2f kH/s/thread)   \r",
 			days, hours, minutes, seconds,
 			hashes_nice, hashes_nice_unit, (hashes_nice > 1000 ? " (!!)" : ""),
 			total_khashes - last_total_khashes,
